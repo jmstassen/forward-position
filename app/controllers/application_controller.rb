bca25a539fe2d108a9154e70101b4bfdd6750746
John M. Stassen <john.stassen@gmail.com>
2021-01-24 09:20:53 -0800

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "this_is_random_salt"
   end
 
   get "/" do
