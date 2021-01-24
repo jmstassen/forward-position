18c6fb6a6e6f1c6f3838583fef8490d42c98e07c
John M. Stassen <john.stassen@gmail.com>
2021-01-23 21:16:53 -0800

    add note model and migration to create notes table

---
+++ db/migrate/20210124051446_create_notes.rb
@@ -0,0 +1,9 @@
+class CreateNotes < ActiveRecord::Migration
+  def change
+    create_table :notes do |t|
+      t.text :content
+      t.integer :task_id
+      t.timestamps null: false
+    end
+  end
+end
