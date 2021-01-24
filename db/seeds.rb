24f18a8b3fd68e279383921461d934823a6f1ab9
John M. Stassen <john.stassen@gmail.com>
2021-01-24 15:37:13 -0800

    correct seed data, rollback, migrate and seed db again

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 john = User.create(name: "John", email: "john@john.com", password: "password")
 long = User.create(name: "Long", email: "long@long.com", password: "password")
 
-task1 = Task.create(name: "Approve timecards", due_date: "2020-02-20", do_date: "2020-02-19", category: "Approval", size: "5", status: "active", user_id: john.id)
-task2 = Task.create(name: "Email Heather", due_date: "2020-01-24", do_date: "2020-01-24", category: "Email", size: "3", status: "active", user_id: john.id)
-task3 = Task.create(name: "New business cards", due_date: "2020-01-30", do_date: "2020-01-25", category: "Deliverable", size: "3", status: "active", user_id: long.id)
+task1 = Task.create(name: "Approve timecards", due_date: "2020-02-20", do_date: "2020-02-19", category: "approval", size: "5", status: "active", user_id: john.id)
+task2 = Task.create(name: "Email Heather", due_date: "2020-01-24", do_date: "2020-01-24", category: "email", size: "3", status: "active", user_id: john.id)
+task3 = Task.create(name: "New business cards", due_date: "2020-01-30", do_date: "2020-01-25", category: "deliverable", size: "3", status: "active", user_id: long.id)
 
 note1 = Note.create(content: "Remind her about something", task_id: task2.id, status: "open", user_id: long.id)
 note2 = Note.create(content: "Look at calendar", task_id: task2.id, status: "open", user_id: long.id)
