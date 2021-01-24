42d1f05739323df2964d64bfb9a81db642e358c4
John M. Stassen <john.stassen@gmail.com>
2021-01-24 13:19:30 -0800

    build out post /tasks route to create new task and new note when applicable

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,34 @@ class TasksController < ApplicationController
   end
 
   post '/tasks' do
-    raise params.inspect
+    # create a new task and save it to the db
+    # create a new note and save it to the db
+    # make sure task and note have all necessary attributes
+    # make sure task and note are only created if user is logged in
+    binding.pry
+    if !logged_in?
+      redirect '/'
+    else
+      if params[:task][:name] != ""
+        @task = Task.create(params[:task])
+        if !params[:note][:content].empty?
+          @task.notes << Note.create(content: params[:note][:content])
+        end
+        redirect "/tasks/#{@task.id}"
+      else
+        redirect '/tasks/new'
+      end
+    end
+
+
+
+    # {"task"=>
+    # {"name"=>"This is a whole new task", 
+    # "due_date"=>"2021-01-28", 
+    # "do_date"=>"2021-01-27", 
+    # "category"=>"fyi", 
+    # "size"=>"5", 
+    # "note"=>{"content"=>"here is a new note within a task"}}}
   end
 
   get '/tasks/:id' do
