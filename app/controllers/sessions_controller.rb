b339c1c6032a8b7e0a42882f288ea3bd1d1a7fa5
John M. Stassen <john.stassen@gmail.com>
2021-02-15 20:01:10 -0800

    remove assistant methods from sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,31 +23,4 @@ class SessionsController < ApplicationController
     session.clear
     redirect '/'
   end
-
-  get '/assistant/tasks' do
-    if !logged_in?
-      redirect '/'
-    else
-      @user = User.find_by(:assistant_email => current_user.email)
-      @tasks = @user.tasks.select {|task| task.status == "active"}
-      session[:assistant] = "yes"
-      erb :'tasks/index'
-    end
-  end
-
-  get '/assistant/references' do
-    if !logged_in?
-      redirect '/'
-    else
-      @user = User.find_by(:assistant_email => current_user.email)
-      @references = @user.references.sort_by { |r| [r.title.downcase]}
-      erb :'references/index'
-    end
-  end
-
-  get '/exit-assistant' do
-    session.delete(:assistant)
-    redirect '/tasks'
-  end
-
 end
